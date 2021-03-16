9daa7b97742651addc6881bf16d68286e7307b86
dreamthinnaphat <6106021612203@fitm.kmutnb.ac.th>
2021-03-16 11:41:25 +0700

    Update

--- test_employee1.py
+++ test_employee1.py
@@ -1,9 +1,27 @@
 import unittest
+from unittest.mock import patch
 from employee1 import Employee
 
 class TestEmployee(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('teardownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.emp_1 = Employee('Corey', 'Schafer', 50000)
+        self.emp_2 = Employee('Sue', 'Smith', 60000)
+
+    def tearDown(self):
+        print('tearDown\n')
     
     def test_email(self):
+        print('test_email')
         emp_1 = Employee('Corey', 'Schafer', 50000)
         emp_2 = Employee('Sue', 'Smith', 60000)
 
@@ -38,6 +56,21 @@ class TestEmployee(unittest.TestCase):
 
         self.assertEqual(emp_1.pay, 52500)
         self.assertEqual(emp_2.pay, 63000)
+
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = 'Success'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/Schafer/May')
+            self.assertEqual(schedule, 'Success')
+
+            mocked_get.return_value.ok = False
+
+            schedule = self.emp_2.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/Smith/June')
+            self.assertEqual(schedule, 'Bad Response!')
 
 if __name__ == '__main__':
     unittest.main()
